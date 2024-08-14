511eef5243a46cd0ccff89871a52a2da7c0edc39
kishore <kishoreram10106@gmail.com>
2024-08-14 23:12:16 +0530

    routes and controller setup

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,11 @@
+export const login = (req,res)=>{
+    res.send("Login Route");
+}
+
+export const signup = (req,res)=>{
+    res.send("signin route")
+}
+
+export const logout = (req,res)=>{
+    res.send("logout route")
+}
--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,10 +1,13 @@
 import express from "express";
+import { login, logout, signup } from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
-router.get("/login",(req,res)=>{
-    res.send("Login Route");
-})
+router.post("/login",login)
+
+router.post("/signup",signup)
+
+router.post("/logout",logout)
 
 
 export default router;
