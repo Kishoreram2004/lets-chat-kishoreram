177b2a25d1e930d302b9b7b3151e5e7e43e92535
kishore <kishoreram10106@gmail.com>
2024-10-17 20:46:48 +0530

    get conversation functionality

--- client/src/components/Conversation.jsx
+++ client/src/components/Conversation.jsx
@@ -1,23 +1,27 @@
 import React from 'react'
+import useConversation from '../zustand/useConversation.js'
 
-const Conversation = () => {
+const Conversation = ({conversation,lastIdx}) => {
+  const {selectedConversation, setSelectedConversation} = useConversation();
+  const isSelected = selectedConversation?._id === conversation._id;
+  
   return (
     <>
-       <div className='flex gap-4 items-center text-white hover:bg-sky-500 rounded p-2 py-1 cursor-pointer'>
+       <div className={`flex gap-4 items-center text-white hover:bg-sky-500 rounded p-2 py-1 cursor-pointer ${isSelected?"bg-sky-500": ""}` } onClick={()=>{setSelectedConversation(conversation)}}>
             <div className='avatar online'>
                 <div className="w-12 rounded-full">
-                <img src="https://static.vecteezy.com/system/resources/thumbnails/002/387/693/small_2x/user-profile-icon-free-vector.jpg" alt="use avatar"></img>
+                <img src={conversation.profilePic} alt="use avatar"></img>
                 </div>
             </div>
 
             <div className='flex flex-col flex-1'>
                 <div className='flex gap-3 justify-between'>
-                    <p className='font-bold'>kishore</p>
-                    <span className="text-xl">^_^</span>
+                    <p className='font-bold'>{conversation.fullName}</p>
+
                 </div>
             </div>
        </div>
-       <div className='divider my-0 py-0 h-1' />
+       {!lastIdx && <div className='divider my-0 py-0 h-1' />}
     </>
   )
 }
--- client/src/components/Conversations.jsx
+++ client/src/components/Conversations.jsx
@@ -1,14 +1,22 @@
 import React from 'react'
 import Conversation from './Conversation.jsx'
+import useGetConversation from '../hooks/useGetConversation.js'
+import { Toaster } from 'react-hot-toast';
 
 const Conversations = () => {
+  const {loading, conversations} = useGetConversation();
+  
   return (
     <div className='flex flex-col py-2 overflow-auto'>
-        <Conversation />
-        <Conversation />
-        <Conversation />
-        <Conversation />
-        <Conversation />
+        {conversations.map((conversation,idx)=>(
+          <Conversation 
+            key={conversation._id}
+            conversation = {conversation}
+            lastIdx = {idx === conversation.length-1}
+          />
+        ))}
+        {loading?<span className='loading loading-spinner mx-auto'></span>:null}
+        <Toaster />
     </div>
   )
 }
