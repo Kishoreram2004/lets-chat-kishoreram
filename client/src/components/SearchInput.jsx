5fda3cc27eab3de26402785962437c443e7c2edf
kishore <kishoreram10106@gmail.com>
2024-12-02 10:46:21 +0530

    search conversation functionality

--- client/src/components/SearchInput.jsx
+++ client/src/components/SearchInput.jsx
@@ -1,13 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { FaSearch } from "react-icons/fa";
+import useConversation from '../zustand/useConversation';
+import useGetConversation from '../hooks/useGetConversation';
+import {Toaster} from 'react-hot-toast';
+import toast from "react-hot-toast"
 
 const SearchInput = () => {
+  const [search,setSearch] = useState("");
+  const {setSelectedConversation} = useConversation();
+  const {conversations} = useGetConversation();
+  const handleSubmit = (e)=>{
+    e.preventDefault()
+    if (!search) return;
+
+    const conversation = conversations.find((c)=>c.fullName.toLowerCase().includes(search.toLowerCase()))
+
+    if (conversation){
+      setSelectedConversation(conversation);
+      setSearch("")
+    }else{
+      toast.error("No such user found!")
+    }
+  }
   return (
-    <form className='flex items-center gap-2'>
-        <input type="text" placeholder="Search.." className='input rounded-full input-bordered'></input>
+    <div>
+      <form onSubmit={handleSubmit} className='flex items-center gap-2'>
+        <input 
+          type="text" 
+          placeholder="Search.." 
+          className='input rounded-full input-bordered'
+          value={search}
+          onChange={(e)=>setSearch(e.target.value)}
+        ></input>
         <button type="submit" className='btn btn-circle border-none hover:bg-sky-400 text-white
         ' ><FaSearch /></button>
-    </form>
+        
+     </form>
+     <Toaster />
+    </div>
+    
   )
 }
 
