668123879ce7350d32e3f20fdbb08cc848558939
kishore <kishoreram10106@gmail.com>
2024-12-05 12:55:00 +0530

    deployment code

--- backend/server.js
+++ backend/server.js
@@ -6,12 +6,12 @@ import userRoutes from "./routes/user.routes.js";
 import connectToDB from "./db/connectToDB.js";
 import cookieParser from "cookie-parser";
 import {app,server} from "./socket/socket.js"
-
+import path from "path"
 dotenv.config();
 
 const PORT= process.env.PORT || 5000;
 
-
+const __dirname = path.resolve();
 
 app.use(express.json())
 
@@ -22,6 +22,12 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages",messageRoutes);
 
 app.use("/api/users",userRoutes);
+
+app.use(express.static(path.join(__dirname,"/client/dist")))
+
+app.get("*",(req,res)=>{
+    res.sendFile(path.join(__dirname, "client","dist","index.html"))
+})
 
 server.listen(PORT,()=>{
     connectToDB();
